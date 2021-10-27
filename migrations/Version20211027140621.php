0adb88682fe5e2cb45985af92e24c3e72eea1b03
lukasrynt <lukasrynt@gmail.com>
2021-10-27 20:38:21 +0200

    Reformat migration #7

--- migrations/Version20211027140621.php
+++ migrations/Version20211027140621.php
@@ -17,7 +17,13 @@ final class Version20211027140621 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE SEQUENCE room_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE room (id INT NOT NULL, capacity INT NOT NULL, name VARCHAR(255) NOT NULL, floor INT NOT NULL, opened_from TIME(0) WITHOUT TIME ZONE NOT NULL, opened_to TIME(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE room (id INT NOT NULL,
+                                              capacity INT NOT NULL,
+                                              name VARCHAR(255) NOT NULL,
+                                              floor INT NOT NULL,
+                                              opened_from TIME(0) WITHOUT TIME ZONE NOT NULL,
+                                              opened_to TIME(0) WITHOUT TIME ZONE NOT NULL,
+                                              PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
