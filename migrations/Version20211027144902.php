f7b03cd224bd16886f92df51a0145bf1312bc281
Martin Šír <sirmart2@fit.cvut.cz>
2021-10-27 16:53:27 +0200

    Add migration for building model

---
+++ migrations/Version20211027144902.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20211027144902 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Creates new table building';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('DROP SEQUENCE room_id_seq CASCADE');
+        $this->addSql('CREATE SEQUENCE building_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE building (id INT NOT NULL, name VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('DROP TABLE room');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE building_id_seq CASCADE');
+        $this->addSql('CREATE SEQUENCE room_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE room (id INT NOT NULL, capacity INT NOT NULL, name VARCHAR(255) NOT NULL, floor INT NOT NULL, opened_from TIME(0) WITHOUT TIME ZONE NOT NULL, opened_to TIME(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('DROP TABLE building');
+    }
+}
