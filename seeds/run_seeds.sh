0fd44ea42bcf9c65111fda01348ea1165d05689e
Martin Sir <sirmart2@fit.cvut.cz>
2021-10-27 15:47:20 +0200

    Modified path to users.sql file

--- seeds/run_seeds.sh
+++ seeds/run_seeds.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-PGPASSWORD=postgres psql -h localhost -p 5432 -d hmsr_db -U postgres -a -f seeds/users.sql
+PGPASSWORD=postgres psql -h localhost -p 5432 -d hmsr_db -U postgres -a -f users.sql
