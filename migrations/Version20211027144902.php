dfdf3b4261e89dd1b90c03fb403614a99787fe39
lukasrynt <lukasrynt@gmail.com>
2021-10-27 20:28:52 +0200

    Fix building migrations #8

--- migrations/Version20211027144902.php
+++ migrations/Version20211027144902.php
@@ -16,18 +16,17 @@ final class Version20211027144902 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('DROP SEQUENCE room_id_seq CASCADE');
         $this->addSql('CREATE SEQUENCE building_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE building (id INT NOT NULL, name VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('DROP TABLE room');
+        $this->addSql('CREATE TABLE building (id INT NOT NULL,
+                                                  name VARCHAR(255) NOT NULL,
+                                                  address VARCHAR(255) NOT NULL,
+                                                  PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE building_id_seq CASCADE');
-        $this->addSql('CREATE SEQUENCE room_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE room (id INT NOT NULL, capacity INT NOT NULL, name VARCHAR(255) NOT NULL, floor INT NOT NULL, opened_from TIME(0) WITHOUT TIME ZONE NOT NULL, opened_to TIME(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('DROP TABLE building');
     }
 }
