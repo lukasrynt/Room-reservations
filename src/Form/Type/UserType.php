62f07d864cf58b803419dd14539fa44123b5bbf0
Marketa Minarova <minarma5@fit.cvut.cz>
2021-11-03 23:22:20 +0100

    Added UserType form, Registration & Login & User controller

---
+++ src/Form/Type/UserType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        return $builder
+            ->add("first_name", TextType::class)
+            ->add("last_name", TextType::class)
+            ->add("email", TextType::class)
+            ->add("phone_number", IntegerType::class)
+            ->add("role", TextType::class)
+            ->add("note", TextType::class)
+            ->add("username", TextType::class)
+            ->add("plain_password", RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Confirm Password']
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
