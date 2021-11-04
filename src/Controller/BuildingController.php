f25d73ba4cd53f6c38d359cf2854841ade1f4273
Martin Šír <sirmart2@fit.cvut.cz>
2021-11-04 20:22:11 +0100

    Add detail and edit method

--- src/Controller/BuildingController.php
+++ src/Controller/BuildingController.php
@@ -5,22 +5,29 @@ namespace App\Controller;
 
 
 
+use App\Form\Type\BuildingType;
 use App\Repository\BuildingRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BuildingController extends AbstractController
 {
     private BuildingRepository $buildingRepository;
+    private EntityManagerInterface $entityManager;
 
     /**
      * BuildingController constructor.
      * @param BuildingRepository $buildingRepository
+     * @param EntityManagerInterface $entityManager
      */
-    public function __construct(BuildingRepository $buildingRepository)
+    public function __construct(BuildingRepository $buildingRepository, EntityManagerInterface $entityManager)
     {
         $this->buildingRepository = $buildingRepository;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -31,5 +38,44 @@ class BuildingController extends AbstractController
     {
         $buildings = $this->buildingRepository->findAll();
         return $this->render('buildings/index.html.twig', ['buildings' => $buildings]);
+    }
+
+    /**
+     * @Route("/buildings/{id}", name="building_detail")
+     * @param int $id
+     * @return Response
+     */
+    public function detail(int $id): Response{
+        $building = $this->buildingRepository->find($id);
+        if (!$building)
+            return $this->render('errors/404.html.twig');
+        return $this->render('buildings/detail.html.twig', ['building' => $building]);
+    }
+
+    /**
+     * @Route("/buildings/{id}/edit", name="building_edit")
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     */
+    public function edit(Request $request, int $id): Response{
+        $building = $this->buildingRepository->find($id);
+
+        if (!$building)
+            return $this->render('errors/404.html.twig');
+
+        $form = $this->createForm(BuildingType::class, $building)
+            ->add('edit', SubmitType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $this->entityManager->persist($form->getData());
+            $this->entityManager->flush();
+            return $this->redirectToRoute('building_detail', ['id' => $building->getId()]);
+        }
+
+        return $this->render('buildings/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
