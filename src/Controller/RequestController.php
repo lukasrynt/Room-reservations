b78e484316037902e9b4d7ba7722dc1fc52ab31e
lukas <ryntluka@fit.cvut.cz>
2021-11-24 23:27:29 +0100

    Add request service

--- src/Controller/RequestController.php
+++ src/Controller/RequestController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\RequestRepository;
+use App\Services\RequestService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,16 +13,16 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class RequestController extends AbstractController
 {
-    private RequestRepository $requestRepository;
+    private RequestService $requestService;
 
     /**
      * RequestController constructor.
-     * @param RequestRepository $requestRepository
+     * @param RequestService $requestService
      * @param EntityManagerInterface $entityManager
      */
-    public function __construct(RequestRepository $requestRepository, EntityManagerInterface $entityManager)
+    public function __construct(RequestService $requestService, EntityManagerInterface $entityManager)
     {
-        $this->requestRepository = $requestRepository;
+        $this->requestService = $requestService;
     }
 
     /**
@@ -32,7 +32,7 @@ class RequestController extends AbstractController
      */
     public function detail(int $id): Response
     {
-        $request = $this->requestRepository->find($id);
+        $request = $this->requestService->find($id);
         if (!$request)
             return $this->render('errors/404.html.twig');
         return $this->render('requests/detail.html.twig', ['request' => $request]);
