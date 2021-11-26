2d56a3594b47d6c3cd1f804d9c9d9acb9a67a668
Marketa Minarova <minarma5@fit.cvut.cz>
2021-11-26 15:03:21 +0100

    Modify EnumRolesType to use Roles class

--- src/Entity/EnumRolesType.php
+++ src/Entity/EnumRolesType.php
@@ -4,16 +4,10 @@ namespace App\Entity;
 
 use Doctrine\DBAL\Types\Type;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
+use App\Entity\Roles;
 
 class EnumRolesType extends Type
 {
-
-    const ROLE = 'ROLE';
-    const ROOM_MEMBER = 'ROOM_MEMBER';
-    const ROOM_ADMIN = 'ROOM_ADMIN';
-    const GROUP_MEMBER = 'GROUP_MEMBER';
-    const GROUP_ADMIN = 'GROUP_ADMIN';
-    const ADMIN = 'ADMIN';
 
     // return the SQL used to create your column type. To create a portable column type, use the $platform.
     public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
@@ -22,25 +16,31 @@ class EnumRolesType extends Type
     }
 
     // This is executed when the value is read from the database. Make your conversions here, optionally using the $platform.
-    public function convertToPHPValue($value, AbstractPlatform $platform)
+    public function convertToPHPValue($value, AbstractPlatform $platform): Roles
     {
-        return $value;
+        if (! Roles::isValid($value)) {
+            throw new \InvalidArgumentException(sprintf(
+                'The value "%s" is not valid for the enum "%s".',
+                $value,
+                Roles::class,
+            ));
+        }
+        return new Roles($value);
     }
 
     // This is executed when the value is written to the database. Make your conversions here, optionally using the $platform.
-    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): string
     {
-        if (!in_array($value, array(self::ROOM_MEMBER, self::ROOM_ADMIN, self::GROUP_MEMBER,
-                                                            self::GROUP_ADMIN, self::ADMIN))) {
+        if (!in_array($value, array("ROOM_MEMBER", "ROOM_ADMIN", "GROUP_MEMBER", "GROUP_ADMIN", "ADMIN"))) {
             throw new \InvalidArgumentException("Invalid role");
         }
-        return $value;
+        return (string) $value;
     }
 
     // modify to match your constant name
     public function getName(): string
     {
-        return self::ROLE;
+        return "enum_roles_type";
     }
 
     public function requiresSQLCommentHint(AbstractPlatform $platform): bool
