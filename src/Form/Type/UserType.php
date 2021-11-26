e632c771168f2e222df49206066466866a089b85
Daniel Honys <honysdan@fit.cvut.cz>
2021-11-26 19:49:13 +0100

    added styling for sign up form

--- src/Form/Type/UserType.php
+++ src/Form/Type/UserType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,17 +17,39 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         return $builder
-            ->add("first_name", TextType::class)
-            ->add("last_name", TextType::class)
-            ->add("email", TextType::class)
-            ->add("phone_number", IntegerType::class)
-            ->add("role", TextType::class)
-            ->add("note", TextType::class)
-            ->add("username", TextType::class)
+            ->add("first_name", TextType::class, [
+                'attr' => ['placeholder' => 'First name']
+            ])
+            ->add("last_name", TextType::class, [
+                'attr' => ['placeholder' => 'Last name']
+            ])
+            ->add("email", TextType::class, [
+                'attr' => ['placeholder' => 'Email']
+            ])
+            // TODO should be TelType
+            ->add("phone_number", IntegerType::class, [
+                'attr' => ['placeholder' => 'Phone number']
+            ])
+            ->add("role", TextType::class, [
+                'attr' => ['placeholder' => 'Role']
+            ])
+            // TODO should be TextareaType
+            ->add("note", TextType::class, [
+                'attr' => ['placeholder' => 'Note']
+            ])
+            ->add("username", TextType::class, [
+                'attr' => ['placeholder' => 'Username']
+            ])
             ->add("plain_password", RepeatedType::class, [
                 'type' => PasswordType::class,
-                'first_options' => ['label' => 'Password'],
-                'second_options' => ['label' => 'Confirm Password']
+                'first_options' => ['label' => 'Password',
+                    'attr' => ['placeholder' => 'Password']],
+                'second_options' => ['label' => 'Confirm Password',
+                    'attr' => ['placeholder' => 'Confirm Password']],
+            ])
+            ->add('save', SubmitType::class, [
+                'attr' => ['class' => 'button-base button-main'],
+                'label' => 'Register'
             ]);
     }
 
