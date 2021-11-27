95a6e57fd1b1b6d068e4d02b469715a49bafd420
Marketa Minarova <minarma5@fit.cvut.cz>
2021-11-27 11:24:56 +0100

    Create Admin entity and AdminRepository

---
+++ src/Entity/Admin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdminRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AdminRepository::class)
+ * @ORM\Table(name="admin")
+ */
+class Admin extends User
+{
+
+}
