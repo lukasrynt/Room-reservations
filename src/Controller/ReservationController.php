52cd0c5d59ecef35b97be321beaa411d8737aec1
Martin Sir <sirmart2@fit.cvut.cz>
2021-11-27 18:32:59 +0100

    Add class route and name

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -10,6 +10,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/reservations", name="reservations_")
+ */
 class ReservationController extends AbstractController
 {
     private ReservationRepository $reservationRepository;
@@ -28,7 +31,7 @@ class ReservationController extends AbstractController
 
 
     /**
-     * @Route("/reservations/new", name="reservation_new")
+     * @Route("/new", name="_new")
      * @return Response
      */
     public function createReservation(): Response
