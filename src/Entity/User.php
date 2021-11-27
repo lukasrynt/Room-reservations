b94c131edb87f3c67ba419e18ae2e348cbbabf02
Marketa Minarova <minarma5@fit.cvut.cz>
2021-11-27 15:10:45 +0100

    Add roomUser to DiscriminationMap and make new migration creating table for decomposition

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -17,7 +17,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
  * @ORM\InheritanceType("SINGLE_TABLE")
  * @ORM\DiscriminatorColumn(name="discr", type="string")
  * @ORM\DiscriminatorMap({"user" = "User", "admin" = "Admin", "roomManager" = "RoomManager",
- *                          "groupManager" = "GroupManager", "groupMember" = "GroupMember"})
+ *                          "groupManager" = "GroupManager", "groupMember" = "GroupMember", "roomUser" = "RoomUser"})
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
