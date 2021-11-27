82c8849e8134bb175b4c38b22404da0cb0f35a4f
Marketa Minarova <minarma5@fit.cvut.cz>
2021-11-27 13:44:02 +0100

    Add and update seeds

---
+++ seeds/groups.sql
@@ -0,0 +1,7 @@
+insert into "group" (id, name, group_manager_id)
+values (
+           nextval('group_id_seq'), 'Pilni studenti', 5
+       ),
+       (
+           nextval('group_id_seq'), 'Ne moc dobri studenti', 5
+       );
--- seeds/rooms.sql
+++ seeds/rooms.sql
@@ -1,7 +1,7 @@
-insert into room (id, capacity, name, floor, opened_from, opened_to, building_id)
+insert into room (id, capacity, name, floor, opened_from, opened_to, building_id, room_manager_id)
 values (
-            nextval('room_id_seq'), 20, '320', 3, '10:00:00', '18:00:00', 1
+            nextval('room_id_seq'), 20, '320', 3, '10:00:00', '18:00:00', 1, 4
        ),
        (
-           nextval('room_id_seq'), 20, '321', 3, '10:00:00', '19:00:00', 2
+           nextval('room_id_seq'), 20, '321', 3, '10:00:00', '19:00:00', 2, null
        );
