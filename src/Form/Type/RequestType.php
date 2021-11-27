a978603f0870e3efbd3a8246763735bdfaa5e4bc
Martin Sir <sirmart2@fit.cvut.cz>
2021-11-27 18:05:23 +0100

    Delete unnecessary code

--- src/Form/Type/RequestType.php
+++ src/Form/Type/RequestType.php
@@ -3,25 +3,16 @@
 
 namespace App\Form\Type;
 
-
-use App\Entity\Room;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class RequestType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        return $builder
+        $builder
             ->add('date_from', DateType::class)
-            ->add('date_to', DateType::class)
-            ->add('valid', DateType::class)
-            ->add('room', EntityType::class,[
-                "class" => Room::class
-            ]);
+            ->add('date_to', DateType::class);
     }
 }
