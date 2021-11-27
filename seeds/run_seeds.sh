7214113e7de253169aa866a9f490865c1b70daa5
Martin Sir <sirmart2@fit.cvut.cz>
2021-11-27 18:28:32 +0100

    Add group script into seeds script

--- seeds/run_seeds.sh
+++ seeds/run_seeds.sh
@@ -2,6 +2,7 @@
 PGPASSWORD=pgpass psql -h localhost -p 5432 -d pgdb -U pguser -a -f truncate.sql
 PGPASSWORD=pgpass psql -h localhost -p 5432 -d pgdb -U pguser -a -f reset_seq.sql
 PGPASSWORD=pgpass psql -h localhost -p 5432 -d pgdb -U pguser -a -f users.sql
+PGPASSWORD=pgpass psql -h localhost -p 5432 -d pgdb -U pguser -a -f groups.sql
 PGPASSWORD=pgpass psql -h localhost -p 5432 -d pgdb -U pguser -a -f buildings.sql
 PGPASSWORD=pgpass psql -h localhost -p 5432 -d pgdb -U pguser -a -f rooms.sql
 PGPASSWORD=pgpass psql -h localhost -p 5432 -d pgdb -U pguser -a -f request.sql
