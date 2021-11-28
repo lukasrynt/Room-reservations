cdc93b0cbd6891e2d22697cfb7e31b71c2fe7fdd
Martin Sir <sirmart2@fit.cvut.cz>
2021-11-28 16:08:20 +0100

    Update ReservationType to filter just selected rooms

--- src/Form/Type/ReservationType.php
+++ src/Form/Type/ReservationType.php
@@ -21,14 +21,26 @@ class ReservationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $rooms = $options['rooms'];
+
         $builder
             ->add('date_from', DateType::class)
             ->add('date_to', DateType::class)
-            ->add('user_id', EntityType::class, [
+            ->add('user', EntityType::class, [
                 "class" => User::class
             ])
-            ->add('room_id', EntityType::class, [
-                "class" => Room::class
+            ->add('room', EntityType::class, [
+                "class" => Room::class,
+                "query_builder" => function (RoomRepository $er) use ($rooms){
+                    return $er->createQueryBuilder('room')
+                        ->andWhere("room.id IN(:rooms)")
+                        ->setParameter('rooms', $rooms);
+                },
             ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired(['rooms']);
     }
 }
