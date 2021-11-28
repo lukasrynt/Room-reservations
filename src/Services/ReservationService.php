0b0ba35ca3396fa27047fd1e941437536992a895
Martin Sir <sirmart2@fit.cvut.cz>
2021-11-28 16:09:43 +0100

    Add reservation service

---
+++ src/Services/ReservationService.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Entity\Request;
+use App\Entity\Reservation;
+use App\Repository\ReservationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ReservationService
+{
+    private ReservationRepository $reservationRepository;
+    private EntityManagerInterface $entityManager;
+
+    /**
+     * ReservationService constructor.
+     * @param ReservationRepository $reservationRepository
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(ReservationRepository $reservationRepository, EntityManagerInterface $entityManager)
+    {
+        $this->reservationRepository = $reservationRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @param int $id
+     * @return Reservation|null
+     */
+    public function find(int $id): ?Reservation
+    {
+        return $this->reservationRepository->find($id);
+    }
+
+    /**
+     * @return Reservation[]|array
+     */
+    public function findAll(): array
+    {
+        return $this->reservationRepository->findAll();
+    }
+
+    public function save(Reservation $reservation): void
+    {
+        $this->entityManager->persist($reservation);
+        $this->entityManager->flush();
+    }
+
+    public function saveFromRequest(Request $request): int
+    {
+        $reservation = new Reservation();
+        $reservation->setRoom($request->getRoom());
+        $reservation->setDateFrom($request->getDateFrom());
+        $reservation->setDateTo($request->getDateTo());
+        $reservation->setUser($request->getRequestor());
+
+        // todo change state of request - ENUM
+        $request->setValid(true);
+
+        $this->save($reservation);
+        return $reservation->getId();
+    }
+}
