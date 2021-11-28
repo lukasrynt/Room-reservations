be5c9f73a411a29872daa6fc8e687b50eb81d047
lukas <ryntluka@fit.cvut.cz>
2021-11-28 01:40:12 +0100

    Add groups to sql script

--- seeds/run_seeds.sh
+++ seeds/run_seeds.sh
@@ -5,3 +5,4 @@ PGPASSWORD=pgpass psql -h localhost -p 5432 -d pgdb -U pguser -a -f users.sql
 PGPASSWORD=pgpass psql -h localhost -p 5432 -d pgdb -U pguser -a -f buildings.sql
 PGPASSWORD=pgpass psql -h localhost -p 5432 -d pgdb -U pguser -a -f rooms.sql
 PGPASSWORD=pgpass psql -h localhost -p 5432 -d pgdb -U pguser -a -f request.sql
+PGPASSWORD=pgpass psql -h localhost -p 5432 -d pgdb -U pguser -a -f groups.sql
