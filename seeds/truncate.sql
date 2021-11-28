f2a05b0defc203942f37f3a7d2875d6efebc0e20
lukas <ryntluka@fit.cvut.cz>
2021-11-28 01:56:30 +0100

    Fix sequence restarts for tables

--- seeds/truncate.sql
+++ seeds/truncate.sql
@@ -1,1 +1,6 @@
-TRUNCATE "user", room, building, room_user, request_user, request
+TRUNCATE "user", room, building, room_user, request_user, request, "group" CASCADE;
+ALTER SEQUENCE user_id_seq RESTART WITH 1;
+ALTER SEQUENCE room_id_seq RESTART WITH 1;
+ALTER SEQUENCE building_id_seq RESTART WITH 1;
+ALTER SEQUENCE request_id_seq RESTART WITH 1;
+ALTER SEQUENCE group_id_seq RESTART WITH 1;
