f00584e34bde53ad89ece1e1194ceea04ed588ab
Daniel Honys <honysdan@fit.cvut.cz>
2021-11-29 22:28:29 +0100

    hamburger menu refactor

--- public/javascript/hamburger-menu.js
+++ public/javascript/hamburger-menu.js
@@ -1,24 +1,42 @@
 class HamburgerMenu {
-    constructor(nav) {
-        this.nav = nav;
-        this.hideButton = nav.getElementsByTagName('button')[0];
+    constructor(main, header) {
+        this.main = main;
+        this.header = header;
+        this.logo = header.getElementsByTagName('h1')[0];
+        this.nav = header.getElementsByTagName('nav')[0];
+        this.hideButton = header.getElementsByTagName('button')[0];
+        this.section = header.getElementsByTagName('section')[0];
+        this.showButton = header.getElementsByTagName('button')[1];
         this.itemList = [];
 
-        let lis = nav.getElementsByTagName('ul')[0].getElementsByTagName('li');
+        let lis = this.nav.getElementsByTagName('ul')[0].getElementsByTagName('li');
         for (const li of lis) {
             this.itemList.push(li.getElementsByTagName('a')[0]);
+        }
+
+        lis = header.getElementsByClassName('dropdown-content')[0].getElementsByTagName('a');
+        for (const a of lis) {
+            this.itemList.push(a);
         }
     }
 
     show() {
-        this.nav.setAttribute('id', 'hamburger-menu');
-        this.hideButton.classList.remove("hamburger-menu-close-hidden");
+        this.main.style.display = 'none';
+        this.header.setAttribute('id', 'hamburger-menu');
+        this.logo.setAttribute('id', 'hamburger-menu-logo');
+        this.hideButton.classList.remove('hamburger-menu-close-hidden');
+        this.section.classList.add('hamburger-menu-user-links');
+        this.showButton.classList.add('hamburger-menu-close-hidden');
         this.itemList.forEach((x) => x.classList.add('hamburger-menu-item'));
     }
 
     hide() {
-        this.nav.removeAttribute('id');
+        this.main.style.display = 'flex';
+        this.header.removeAttribute('id');
+        this.logo.removeAttribute('id', 'hamburger-menu-logo');
         this.hideButton.classList.add("hamburger-menu-close-hidden");
+        this.section.classList.remove('hamburger-menu-user-links');
+        this.showButton.classList.remove('hamburger-menu-close-hidden');
         this.itemList.forEach((x) => x.classList.remove('hamburger-menu-item'));
     }
 }
