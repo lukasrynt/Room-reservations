cb5d4bcae1e46e316176e0c471cf8a8cd0e7028d
lukas <ryntluka@fit.cvut.cz>
2021-12-01 21:09:25 +0100

    User rest annotations in group api controller

--- src/Controller/Api/GroupController.php
+++ src/Controller/Api/GroupController.php
@@ -7,6 +7,7 @@ namespace App\Controller\Api;
 
 use App\Services\GroupService;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,7 +30,7 @@ class GroupController extends AbstractFOSRestController
     }
 
     /**
-     * @Route("/", methods={"GET"}, name="list")
+     * @Rest\Get("/", name="list")
      * @param Request $request
      * @return Response
      */
@@ -41,8 +42,7 @@ class GroupController extends AbstractFOSRestController
     }
 
     /**
-     * @Route("/{gid}/users/{uid}",
-     *        methods={"PUT"},
+     * @Rest\Put("/{gid}/users/{uid}",
      *        name="add_user",
      *        requirements={"gid": "\d+", "uid": "\d+"})
      * @param int $gid
@@ -57,8 +57,7 @@ class GroupController extends AbstractFOSRestController
     }
 
     /**
-     * @Route("/{gid}/users/{uid}",
-     *        methods={"DELETE"},
+     * @Rest\Delete("/{gid}/users/{uid}",
      *        name="remove_user",
      *        requirements={"gid": "\d+", "uid": "\d+"})
      * @param int $gid
@@ -73,8 +72,7 @@ class GroupController extends AbstractFOSRestController
     }
 
     /**
-     * @Route("/{gid}/rooms/{rid}",
-     *        methods={"PUT"},
+     * @Rest\Put("/{gid}/rooms/{rid}",
      *        name="add_room",
      *        requirements={"gid": "\d+", "rid": "\d+"})
      * @param int $gid
@@ -89,8 +87,7 @@ class GroupController extends AbstractFOSRestController
     }
 
     /**
-     * @Route("/{gid}/rooms/{rid}",
-     *        methods={"DELETE"},
+     * @Rest\Delete("/{gid}/rooms/{rid}",
      *        name="remove_room",
      *        requirements={"gid": "\d+", "rid": "\d+"})
      * @param int $gid
