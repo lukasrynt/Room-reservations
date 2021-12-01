c78c00ce6f263f464f061fa932d703f394850e7e
lukas <ryntluka@fit.cvut.cz>
2021-12-01 20:04:13 +0100

    Refactor migrations

--- migrations/Version20211127113012.php
+++ migrations/Version20211127113012.php
@@ -19,7 +19,6 @@ final class Version20211127113012 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE reservation_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE reservation (id INT NOT NULL, user_id INT NOT NULL, room_id INT NOT NULL, date_to TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, date_from TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_42C84955A76ED395 ON reservation (user_id)');
@@ -34,8 +33,6 @@ final class Version20211127113012 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE reservation_id_seq CASCADE');
         $this->addSql('DROP TABLE reservation');
         $this->addSql('ALTER TABLE "user" ALTER role TYPE VARCHAR(20)');
