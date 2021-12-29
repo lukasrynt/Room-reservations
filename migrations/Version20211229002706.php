9c1bf03a266904499c42eba8bf2150da2f7a82e6
Marketa Minarova <minarma5@fit.cvut.cz>
2021-12-29 01:40:31 +0100

    Add new migration which adds children and parent field to Group table

---
+++ migrations/Version20211229002706.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211229002706 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add children and parent attributes to Group table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE "group" ADD parent_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE "group" ADD CONSTRAINT FK_6DC044C5727ACA70 FOREIGN KEY (parent_id) REFERENCES "group" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_6DC044C5727ACA70 ON "group" (parent_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE "group" DROP CONSTRAINT FK_6DC044C5727ACA70');
+        $this->addSql('DROP INDEX IDX_6DC044C5727ACA70');
+        $this->addSql('ALTER TABLE "group" DROP parent_id');
+    }
+}
