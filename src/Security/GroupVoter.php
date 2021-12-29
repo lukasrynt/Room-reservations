ba0c04b9b5837fa52b6c42e16c78e10a6fb3a7be
Marketa Minarova <minarma5@fit.cvut.cz>
2021-12-29 13:00:47 +0100

    Create GroupVoter

---
+++ src/Security/GroupVoter.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\Group;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class GroupVoter extends Voter
+{
+    const VIEW_ALL = 'view_groups';
+    const VIEW = 'view_group';
+    const EDIT = 'edit_group';
+    const CREATE = 'create_group';
+
+    protected function supports(string $attribute, $subject): bool
+    {
+        if (!in_array($attribute, [self::VIEW, self::EDIT, self::CREATE, self::VIEW_ALL])) {
+            return false;
+        }
+        if (!($subject instanceof Group || !$subject)) {
+            return false;
+        }
+        return true;
+    }
+
+    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        if (!$user) {
+            return false;
+        }
+
+        switch($attribute) {
+            case self::VIEW_ALL:
+                return $this->canViewAll();
+            case self::VIEW:
+                return $this->canView($user, $subject);
+            case self::EDIT:
+                return $this->canEdit($user, $subject);
+            case self::CREATE:
+                return $this->canCreate($user);
+            default:
+                return false;
+        }    }
+
+    private function canViewAll(): bool
+    {
+        return true;
+    }
+
+    private function canView(User $account, Group $group): bool
+    {
+        if ($account->isAdmin()) {
+            return true;
+        }
+
+        return $account === $group->getGroupManager();
+    }
+
+    private function canEdit(User $account, Group $group): bool
+    {
+        return $this->canView($account, $group);
+    }
+
+    private function canCreate(User $account): bool
+    {
+        return $account->isAdmin();
+    }
+}
