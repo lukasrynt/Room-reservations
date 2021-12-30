ed70365d7b7de340227b110d27c22357ea6a8562
Marketa Minarova <minarma5@fit.cvut.cz>
2021-12-30 01:40:19 +0100

    Add rooms, parent and groupManager fields

--- src/Form/Type/GroupType.php
+++ src/Form/Type/GroupType.php
@@ -2,6 +2,11 @@
 
 namespace App\Form\Type;
 
+use App\Entity\Group;
+use App\Entity\GroupManager;
+use App\Entity\Room;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -14,7 +19,29 @@ class GroupType extends AbstractType
         return $builder
             ->add('name', TextType::class, [
                 'label' => 'Group name :',
-            ])->add('edit', SubmitType::class, [
+            ])
+            ->add('groupManager', EntityType::class, [
+                # TODO change GroupManager to User so that anyone can become GroupManager - somehow cast User to GroupManager in GroupController
+                "class" => GroupManager::class,
+                'label' => 'Group Manager :',
+                'placeholder' => 'Choose an option',
+                'required' => false,
+            ])
+            ->add('rooms', EntityType::class, [
+                # TODO selecting rooms doesn't work
+                "class" => Room::class,
+                'label' => 'Room :',
+                'required' => false,
+                'multiple' => true,
+            ])
+            ->add('parent', EntityType::class, [
+                "class" => Group::class,
+                'choice_label' => 'name',
+                'label' => 'Parent group :',
+                'placeholder' => 'Choose an option',
+                'required' => false,
+            ])
+            ->add('edit', SubmitType::class, [
                 'attr' => ['class' => 'button-base button-success'],
                 'label' => 'Save'
             ]);
