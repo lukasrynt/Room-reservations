fb5ddefa89cdbe5617ac56e34057dbb62d47b9ca
lukas <ryntluka@fit.cvut.cz>
2021-12-31 11:05:34 +0100

    Add delete modal to delete button

--- public/javascript/delete.js
+++ public/javascript/delete.js
@@ -1,9 +1,24 @@
+function buildMessage(element) {
+    let action = element.getAttribute('data-confirm');
+    let name = element.getAttribute('data-name');
+    return 'Are you sure you want to ' + action + ' ' + name + '?';
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     let deleteLink = document.getElementsByClassName('delete-link')[0];
     if (deleteLink) {
         deleteLink.addEventListener('click', (e) => {
-            let form = deleteLink.parentElement.getElementsByTagName('form')[0];
-            form.submit();
+            let modal = document.getElementById('delete-modal');
+            document.getElementById('delete-modal-message').innerText = buildMessage(deleteLink)
+            modal.showModal();
+            document.getElementById('delete-modal-cancel').addEventListener('click', () => {
+                modal.close();
+            });
+            document.getElementById('delete-modal-confirm').addEventListener('click', () => {
+                modal.close();
+                let form = deleteLink.parentElement.getElementsByTagName('form')[0];
+                form.submit();
+            });
         })
     }
 });
