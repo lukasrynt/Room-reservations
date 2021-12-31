9493cd81979bef2f412d7e0994224e5af04b4da1
Martin Sir <sirmart2@fit.cvut.cz>
2021-12-31 13:56:39 +0100

    Add method to lock/unlock/open door

--- src/Controller/Api/RoomController.php
+++ src/Controller/Api/RoomController.php
@@ -5,8 +5,12 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Room;
+use App\Entity\User;
 use App\Repository\RoomRepository;
+use App\Services\ReservationService;
 use App\Services\RoomService;
+use App\Services\UserService;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,14 +25,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class RoomController extends AbstractFOSRestController
 {
     private RoomService $roomService;
+    private UserService $userService;
+    private ReservationService $reservationService;
 
     /**
+     * RoomController constructor.
      * @param RoomService $roomService
+     * @param UserService $userService
+     * @param ReservationService $reservationService
      */
-    public function __construct(RoomService $roomService)
+    public function __construct(RoomService $roomService, UserService $userService, ReservationService $reservationService)
     {
         $this->roomService = $roomService;
+        $this->userService = $userService;
+        $this->reservationService = $reservationService;
     }
+
 
     /**
      * @Rest\Get("/", name="list")
@@ -52,5 +64,58 @@ class RoomController extends AbstractFOSRestController
         $request = $this->roomService->find($id);
         $view = $this->view($request, Response::HTTP_OK);
         return $this->handleView($view);
+    }
+
+    /**
+     * @Rest\Put("/{id}/door", name="lock_unlock_door")
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     */
+    public function lockUnlockDoor(Request $request, int $id): Response
+    {
+        $userId = $request->get('user_id');
+        $user = $this->userService->find($userId);
+        $room = $this->roomService->find($id);
+
+        if (!$this->hasUserPermission($user, $room))
+            return $this->handleView($this->view("Access forbidden", Response::HTTP_FORBIDDEN));
+
+        if ($room->getAccessCounter() == 1 && (!$room->getLastAccess() || $room->getLastAccess() == $userId))
+        {
+            $room->setAccessCounter(0);
+            if ($room->getLocked())
+            {
+                $room->setLocked(false);
+                $message = "Unlocking the door";
+            }else {
+                $room->setLocked(true);
+                $message = "Locking the door";
+            }
+
+            $this->roomService->save($room);
+            return $this->handleView($this->view($message, Response::HTTP_OK));
+        }
+
+        $room->setAccessCounter(1);
+        $room->setLastAccess($userId);
+        $this->roomService->save($room);
+        if (!$room->getLocked())
+            return $this->handleView($this->view("Opening the door", Response::HTTP_OK));
+        return $this->handleView($this->view("Access counter to unlock the door: 1/2", Response::HTTP_OK));
+    }
+
+    private function hasUserPermission(User $user, Room $room): bool
+    {
+        if ($user->isAdmin() ||
+            $room->getRoomManager() === $user ||
+            ($user->isGroupAdmin() && $room->getGroup() === $user->getGroup()))
+            return true;
+
+        $currentReservation = $this->reservationService->getCurrentReservation($room);
+        if (!$currentReservation)
+            return false;
+
+        return $currentReservation->getAttendees()->contains($user) || $currentReservation->getUser() === $user;
     }
 }
