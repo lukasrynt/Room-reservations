2a0ff1d73d0bbd7eafaaa90631dca7be8bf1f95e
Daniel Honys <honysdan@fit.cvut.cz>
2022-01-01 22:11:39 +0100

    deploy script

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Hello world"
+docker-compose up -d
+composer install --no-interaction
+php ./bin/console --no-interaction doctrine:migrations:migrate
+cd ./seeds
+bash run_seeds.sh
+cd ..
+symfony server:start -d
